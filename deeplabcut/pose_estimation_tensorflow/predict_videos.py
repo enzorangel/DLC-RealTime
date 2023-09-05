a7b95ff35b239f47d4fb2f9592d81076ec6f9711
enzorangel <enzo.r@ufrn.edu.br>
2023-09-04 23:17:41 -0300

    Frame-single

--- deeplabcut/pose_estimation_tensorflow/predict_videos.py
+++ deeplabcut/pose_estimation_tensorflow/predict_videos.py
@@ -814,8 +814,6 @@ def GetPoseS(cfg, dlc_cfg, sess, inputs, outputs, cap, nframes):
             #     break
         counter += 1
 
-        print(counter)
-
     # pbar.close()
     return PredictedData, nframes
 
